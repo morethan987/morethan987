51bf3de6be0739d2e218661784f5d142d23b24c4
morethan987 <2404385626@qq.com>
2025-11-16 14:45:15 +0800

    AutoClick init

---
+++ AutoClicks/README.md
@@ -0,0 +1,3 @@
+# AutoClicks
+
+这里面收集一些简单的自动化操作脚本，主要基于seleniumbase，推荐使用 uv 进行 python 包管理
---
+++ AutoClicks/main.py
@@ -0,0 +1,7 @@
+def main():
+    print("Hello from autoclicks!")
+    print("Step into the sub-modules to see their functionality.")
+
+
+if __name__ == "__main__":
+    main()
