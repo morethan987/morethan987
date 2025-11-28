368b85de15887fdf768d8ba006c8385f7a37bd8d
morethan987 <2404385626@qq.com>
2025-11-28 19:36:30 +0800

    最终系统框架搭建

---
+++ Java_prac/GradeSystem/GradeSystemBackend/src/main/java/com/example/GradeSystemBackend/GradeSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.GradeSystemBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeSystemBackendApplication.class, args);
+	}
+
+}
---
+++ Java_prac/GradeSystem/GradeSystemBackend/src/test/java/com/example/GradeSystemBackend/GradeSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GradeSystemBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
